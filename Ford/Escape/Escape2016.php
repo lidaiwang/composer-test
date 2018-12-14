4ede01933fe49630fcce6b5d42718e442fd0a0db
class_li <lidaiwangtime@163.com>
2018-12-14 10:26:19 +0800

    composer test

---
+++ Ford/Escape/Escape2016.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EDZ
+ * Date: 2018/12/14
+ * Time: 10:21
+ */
+
+
+namespace Ford\Escape;
+
+class Escape2016
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2016!<br />";
+    }
+}
